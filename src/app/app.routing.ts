6cffc93098738edac90d3c04b5ae8781f66b714d
Andrey <andrey@MacBook-Pro-Andrey.local>
2016-10-21 21:28:31 +0300

    first to start

---
+++ src/app/app.routing.ts
@@ -0,0 +1,11 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './header/header.component';
+
+// const routes: Routes = [
+//   { path: '', component: HomeComponent },
+//   { path: 'header', component: AboutComponent}
+// ];
+
+// export const routing = RouterModule.forRoot(routes);
